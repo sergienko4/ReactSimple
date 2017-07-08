1efbf1f56ec725a4745a0f3dee65155cb5331a29
Eugene Sergienko <Sergienko4@gmail.com>
2017-07-08 22:36:43 +0300

    added component & input UI & connected to Youtube

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
+// my scripts 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyB9LhgWi9_FEJYvVkcuIDd7BlM6xU4Mi8s';
+
+YTSearch({key: API_KEY, term:'surfboards', function(data){
+  console.log('====================================');
+  console.log(data);
+  console.log('====================================');
+}});
+
 // Create a new component. This component should produce some HTML
-import React from 'react'; 
-import ReactDOM  from 'react-dom';
 
 const App = () => {
-  return <div> Hi There! </div>;
-}
+    return (
+    <div> 
+      <SearchBar />
+    </div>
+    );
+};
 
 
 // Take this component's generate HTML and put it on the page ( in DOM)
 
-ReactDOM.render(<App />, document.querySelector('.container'));
+ReactDOM.render( <App/> , document.querySelector('.container'));
